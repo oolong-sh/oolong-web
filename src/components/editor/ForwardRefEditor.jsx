2ece574286f7e40f66be4acd8d758ca1c9005a75
CM360 <31625347+cm-360@users.noreply.github.com>
2024-11-07 18:57:51 -0500

    Styling improvements

--- src/components/editor/ForwardRefEditor.jsx
+++ src/components/editor/ForwardRefEditor.jsx
@@ -2,5 +2,7 @@ import { forwardRef } from 'react';
 
 import Editor from './InitializedMDXEditor';
 
+import './Editor.css';
+
 const ForwardRefEditor = forwardRef((props, ref) => <Editor {...props} editorRef={ref} />);
 export default ForwardRefEditor;
