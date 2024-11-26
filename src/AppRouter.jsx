72e6ca13e6a27cfbf25221dccd997ba57500e332
CM360 <31625347+cm-360@users.noreply.github.com>
2024-11-26 18:14:55 -0500

    Fix conditional router usage (electron/web)

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createHashRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom';
+import isElectron from 'is-electron';
 
 import App from './App';
 import NotesView from './components/views/note-tabs/Tabber';
 import GraphView from './components/views/graph/Graph';
 
 export default function AppRouter() {
-  const router = createHashRouter(
+  const router = createRouter(
     createRoutesFromElements(
       <Route path='/' element={<App />}>
         <Route path='' element={<Navigate to='/notes' />} />
@@ -18,4 +19,10 @@ export default function AppRouter() {
   return (
     <RouterProvider router={router} />
   );
+}
+
+function createRouter(routes) {
+  return isElectron()
+    ? createHashRouter(routes)
+    : createBrowserRouter(routes);
 }
