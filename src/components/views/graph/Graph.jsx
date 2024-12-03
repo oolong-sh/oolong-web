eabdbf4521cc38ea30df19085daf635f1756db6e
CM360 <31625347+cm-360@users.noreply.github.com>
2024-12-02 20:20:52 -0500

    placeholder graph color changes

--- src/components/views/graph/Graph.jsx
+++ src/components/views/graph/Graph.jsx
@@ -9,6 +9,28 @@ function defaultGraphData() {
   }
 }
 
+const loColor = [61, 52, 44];
+const hiColor = [215, 196, 132];
+
+function styleGraphData(graphData) {
+  const { nodes, links } = graphData;
+
+  const newLinks = links.map(link => {
+    // TODO magic number, normalize strength in Go code
+    const strength = link.strength / 0.4;
+    const color = [
+      Math.round(loColor[0] * (1 - strength)) + (hiColor[0] * strength),
+      Math.round(loColor[1] * (1 - strength)) + (hiColor[1] * strength),
+      Math.round(loColor[2] * (1 - strength)) + (hiColor[2] * strength),
+    ];
+    const colorRGB = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+
+    return {...link, color: colorRGB};
+  });
+
+  return {nodes, links: newLinks};
+}
+
 export default function Graph() {
   const [graphData, setGraphData] = useState(defaultGraphData);
 
@@ -18,7 +40,7 @@ export default function Graph() {
       .then(responseJson => {
         // TODO process data before usage?
         console.log(responseJson);
-        setGraphData(responseJson);
+        setGraphData(styleGraphData(responseJson));
       })
       .catch(error => console.error(error));
   }, [setGraphData]);
